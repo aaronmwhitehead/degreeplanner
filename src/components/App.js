41e6561394d582bb1d7ec8660615fc773d847efd
Aaron Whitehead <aaronwhitehead@Aarons-MacBook-Pro.local>
2018-10-29 00:04:19 -0500

    Set up file structure for React App

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles//css/App.css';
+
+class App extends Component {
+  render() {
+    return(
+      <div className="name">Aaron</div>
+    );
+  }
+}
+
+export default App;
